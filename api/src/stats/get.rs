3a7808a2437e27ae27a9c8db4549f905aa2e08da
vincentserpoul <vincent_serpoul@manulife.com>
2022-03-08 10:54:30 +0800

    fix: inner instead of left for health_data join

--- api/src/stats/get.rs
+++ api/src/stats/get.rs
@@ -269,7 +269,7 @@ pub async fn get_db(pool: &PgPool, filters: &GetFilters) -> eyre::Result<Stats> 
                         CURRENT_DATE, 
                         '1 day'
                     ) d
-                    LEFT JOIN (user_session us LEFT JOIN health_data hd ON hd.user_session_id = us.id AND hd.kiosk_location = ANY($1))
+                    LEFT JOIN (user_session us INNER JOIN health_data hd ON hd.user_session_id = us.id AND hd.kiosk_location = ANY($1))
                         ON TO_CHAR(us.created_at AT TIME ZONE 'Asia/Singapore', 'YYYY-MM-DD') = TO_CHAR(d AT TIME ZONE 'Asia/Singapore', 'YYYY-MM-DD')
                     GROUP BY date
                     ORDER BY date
